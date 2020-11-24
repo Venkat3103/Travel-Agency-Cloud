7a9b99ef5b0dd08b111d999ab9a0ca12a4b93584
Venkat3103 <53090670+Venkat3103@users.noreply.github.com>
2020-11-24 18:33:12 +0530

    Create init.sql

---
+++ src/Backend/init.sql
@@ -0,0 +1,9 @@
+ use toursandtravelsagency;
+ create table Users(
+ FirstName varchar(30),
+ LastName varchar(30),
+ ContactNumber bigint,
+ email varchar(30),
+ Sdate varchar(8),
+ packageID varchar(6)
+ );
